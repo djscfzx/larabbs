8f208b119ac0a0a826d498c3c9a70a3e696a7948
vagrant <vagrant@homestead>
2019-03-16 16:55:50 +0000

    修复 N+1 问题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -9,49 +9,41 @@ use App\Http\Requests\TopicRequest;
 
 class TopicsController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth', ['except' => ['index', 'show']]);
-    }
+  public function __construct() {
+    $this->middleware('auth', ['except' => ['index', 'show']]);
+  }
 
-	public function index()
-	{
-		$topics = Topic::paginate();
+	public function index() {
+		$topics = Topic::with('user', 'category')->paginate(30);
 		return view('topics.index', compact('topics'));
 	}
 
-    public function show(Topic $topic)
-    {
-        return view('topics.show', compact('topic'));
-    }
+  public function show(Topic $topic) {
+    return view('topics.show', compact('topic'));
+  }
 
-	public function create(Topic $topic)
-	{
+	public function create(Topic $topic) {
 		return view('topics.create_and_edit', compact('topic'));
 	}
 
-	public function store(TopicRequest $request)
-	{
+	public function store(TopicRequest $request) {
 		$topic = Topic::create($request->all());
 		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
 	}
 
-	public function edit(Topic $topic)
-	{
-        $this->authorize('update', $topic);
+	public function edit(Topic $topic) {
+    $this->authorize('update', $topic);
 		return view('topics.create_and_edit', compact('topic'));
 	}
 
-	public function update(TopicRequest $request, Topic $topic)
-	{
+	public function update(TopicRequest $request, Topic $topic) {
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
 		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
 	}
 
-	public function destroy(Topic $topic)
-	{
+	public function destroy(Topic $topic) {
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
