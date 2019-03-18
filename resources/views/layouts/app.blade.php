df7cfc0426de40262c826c1ddb9f36ac05d6fab3
vagrant <vagrant@homestead>
2019-03-18 15:54:27 +0000

    话题内容页面

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
   <!-- 防止跨域攻击 -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>@yield('title', 'LaraBBS')</title>
+  <meta name="description" content="@yield('description', 'LaraBBS')" />
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
   @yield('styles')
 </head>
