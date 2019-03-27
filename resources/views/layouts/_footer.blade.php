9176898b7d9d36359e5c18f1aa928b47bdc97ee3
vagrant <vagrant@homestead>
2019-03-27 15:09:28 +0000

    user can register using phone

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      Design & Coding By <a href="http://obscure-beach-27737.herokuapp.com" target="_blank">DJSCFZX</a>
+      <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:{{ setting('contact_email') }}">联系我</a></p>
+  </div>
+</footer>
---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', [
+    'namespace' => 'App\Http\Controllers\Api'
+], function($api) {
+    // 短信验证码
+    $api->post('verificationCodes', 'VerificationCodesController@store')
+        ->name('api.verificationCodes.store');
+    // 用户注册
+    $api->post('users', 'UsersController@store')
+        ->name('api.users.store');
+});
