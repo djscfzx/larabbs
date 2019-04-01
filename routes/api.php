00c189c53e41d4fdbf0526bb08acde1997e693d1
vagrant <vagrant@homestead>
2019-04-01 10:02:42 +0000

    topic update api

--- routes/api.php
+++ routes/api.php
@@ -19,52 +19,57 @@ use Illuminate\Http\Request;
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
-  'namespace' => 'App\Http\Controllers\Api',
-  'middleware' => 'serializer:array',
+    'namespace' => 'App\Http\Controllers\Api',
+    'middleware' => ['serializer:array', 'bindings']
 ], function($api) {
-  /*$api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
-  $api->post('users', 'UsersController@store')
-        ->name('api.users.store');*/
-
-  $api->group([
-    'middleware' => 'api.throttle',
-    'limit' => config('api.rate_limits.sign.limit'),
-    'expires' => config('api.rate_limits.sign.expires'),
-  ], function($api) {
-    $api->post('captchas', 'CaptchasController@store')
-        ->name('api.captchas.store');
-    $api->post('verificationCodes', 'VerificationCodesController@store')
+    /*$api->post('verificationCodes', 'VerificationCodesController@store')
         ->name('api.verificationCodes.store');
     $api->post('users', 'UsersController@store')
-        ->name('api.users.store');
-    // 第三方登录
-    $api->post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
-        ->name('api.socials.authorizations.store');
-    // 登录
-    $api->post('authorizations', 'AuthorizationsController@store')
-        ->name('api.authorizations.store');
-    // 刷新token
-    $api->put('authorizations/current', 'AuthorizationsController@update')
-        ->name('api.authorizations.update');
-    // 删除token
-    $api->delete('authorizations/current', 'AuthorizationsController@destroy')
-        ->name('api.authorizations.destroy');
-    // 游客可以访问的接口
-    $api->get('categories', 'CategoriesController@index')
-        ->name('api.categories.index');
-    // 需要 token 验证的接口
-    $api->group(['middleware' => 'api.auth'], function($api) {
-        // 当前登录用户信息
-        $api->get('user', 'UsersController@me')
-            ->name('api.user.show');
-        // 编辑登录用户信息
-        $api->patch('user', 'UsersController@update')
-            ->name('api.user.update');
-        // 图片资源
-        $api->post('images', 'ImagesController@store')
-            ->name('api.images.store');
+        ->name('api.users.store');*/
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ], function($api) {
+        $api->post('captchas', 'CaptchasController@store')
+            ->name('api.captchas.store');
+        $api->post('verificationCodes', 'VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
+        $api->post('users', 'UsersController@store')
+            ->name('api.users.store');
+        // 第三方登录
+        $api->post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
+            ->name('api.socials.authorizations.store');
+        // 登录
+        $api->post('authorizations', 'AuthorizationsController@store')
+            ->name('api.authorizations.store');
+        // 刷新token
+        $api->put('authorizations/current', 'AuthorizationsController@update')
+            ->name('api.authorizations.update');
+        // 删除token
+        $api->delete('authorizations/current', 'AuthorizationsController@destroy')
+            ->name('api.authorizations.destroy');
+        // 游客可以访问的接口
+        $api->get('categories', 'CategoriesController@index')
+            ->name('api.categories.index');
+        // 需要 token 验证的接口
+        $api->group([
+            'middleware' => 'api.auth'
+        ], function($api) {
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
+            // 编辑登录用户信息
+            $api->patch('user', 'UsersController@update')
+                ->name('api.user.update');
+            // 图片资源
+            $api->post('images', 'ImagesController@store')
+                ->name('api.images.store');
+            // 发布话题
+            $api->post('topics', 'TopicsController@store')
+                ->name('api.topics.store');
+            $api->patch('topics/{topic}', 'TopicsController@update')
+                ->name('api.topics.update');
+        });
     });
-  });
-
 });
