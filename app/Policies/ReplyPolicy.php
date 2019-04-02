9a815c97ce29a6b7b4a72e5f4426120632550c93
vagrant <vagrant@homestead>
2019-04-02 06:46:08 +0000

    replies destroy

--- app/Policies/ReplyPolicy.php
+++ app/Policies/ReplyPolicy.php
@@ -7,12 +7,12 @@ use App\Models\Reply;
 
 class ReplyPolicy extends Policy
 {
-  public function update(User $user, Reply $reply) {
-    // return $reply->user_id == $user->id;
-    return true;
-  }
+    public function update(User $user, Reply $reply) {
+        // return $reply->user_id == $user->id;
+        return true;
+    }
 
-  public function destroy(User $user, Reply $reply) {
-    return $user->isAuthorOf($reply) || $user->isAuthorOf($reply->topic);
-  }
+    public function destroy(User $user, Reply $reply) {
+        return $user->isAuthorOf($reply) || $user->isAuthorOf($reply->topic);
+    }
 }
