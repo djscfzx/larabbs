15d2044dfcddde0c105f9fc1772be493258e4204
vagrant <vagrant@homestead>
2019-04-02 06:34:41 +0000

    replies store

--- app/Http/Requests/Api/VerificationCodeRequest.php
+++ app/Http/Requests/Api/VerificationCodeRequest.php
@@ -2,15 +2,8 @@
 
 namespace App\Http\Requests\Api;
 
-// use Illuminate\Foundation\Http\FormRequest;
-use Dingo\Api\Http\FormRequest;
-
 class VerificationCodeRequest extends FormRequest
 {
-    public function authorize() {
-        return true;
-    }
-
     public function rules() {
         return [
             'captcha_key' => 'required|string',
